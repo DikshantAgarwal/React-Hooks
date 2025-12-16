afd55905bc85e4acb8be03ba59e31daba402b700
Dikshant Agarwal <dikshantagarwal234@gmail.com>
2025-12-16 23:55:04 +0530

    Update useScrollPosition.ks hook

--- src/hooks/useScrollPosition.js
+++ src/hooks/useScrollPosition.js
@@ -13,19 +13,22 @@ function useScrollPosition(options = { target: null }) {
 
 //   console.log("Target:", resolveTarget(), options.target);
   const [scrollX, setScrollX] = useState(
-    resolveTarget() === window ? window.scrollX : resolveTarget()?.scrollLeft || 0
+    resolveTarget() === window ? window.scrollX : resolveTarget().scrollLeft || 0
   );
   const [scrollY, setScrollY] = useState(
-    resolveTarget() === window ? window.scrollY : resolveTarget()?.scrollTop || 0
+    resolveTarget() === window ? window.scrollY : resolveTarget().scrollTop || 0
   );
 
 const prev = previousY.current ?? scrollY;
-console.log("Prev:", prev, "Current:", scrollY);
+
 direction = scrollY > prev ? "down" : scrollY < prev ? "up" : "none";
 previousY.current = scrollY;
+
 const listener = useCallback(() => {
+
     if (frameRef.current) return;
     const el = resolveTarget();
+    console.log("Listening to scroll on:", el);
     frameRef.current = requestAnimationFrame(() => {
       const currentX =
         el === window ? window.scrollX : el?.scrollLeft || 0;
